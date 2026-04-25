ed6e41a4319a3b1ffb5887d4d4fce2ceabd33c44
Ahmed Ibrahim <za90.yddgio@gmail.com>
2026-04-25 23:59:07 +0300

    Hard reset all paths

--- src/Dashbourd/page/HowItWorks.jsx
+++ src/Dashbourd/page/HowItWorks.jsx
@@ -1,6 +1,6 @@
 import Title from "../../component/Title";
 import { useTranslation } from "react-i18next";
-import HowitworkInput from "../component/HowItWork/howitworkInput";
+import HowitworkInput from "../component/HowItWork/HowitworkInput";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { getHowItWork } from "../Redux/HowItWork";
